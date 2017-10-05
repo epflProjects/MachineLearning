6cd10d095d7aca638fade44ff80e7d7987f6aaf7
Arnaud Pannatier <arn.pannatier@gmail.com>
2017-10-05 16:31:37 +0200

    Adding of the file "implementations.py" required by the project with the name of the function that we'll have to implement"

---
+++ implementations.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Authors : Matteo Giorla, Bastian Nanchen, Arnaud Pannatier
+# EPFL Fall 2017
+# Machine Learning Course
+# Project : 1
+"""Basic function of Machine Learning Project 1."""
+
+def least_squares_GD(y, tx, initial w, max iters, gamma):
+	"""Linear regression using gradient descent"""
+	raise NotImplementedError
+
+def least_squares_SGD(y, tx, initial w, max iters, gamma):
+	"""Linear regression using stochastic gradient descent"""
+	raise NotImplementedError
+
+def least_squares(y, tx):
+	"""Least squares regression using normal equations"""
+	raise NotImplementedError
+
+def ridge_regression(y, tx, lambda_):
+	"""Ridge regression using normal equations"""
+	raise NotImplementedError
+
+def logistic_regression(y, tx, initial w, max iters, gamma):
+	"""Logistic regression using gradient descent or SGD"""
+	raise NotImplementedError
+	
+def reg_logistic_regression(y, tx, lambda_, initial w, max iters, gamma):
+	"""Regularized logistic regression using gradient descent or SGD"""
+	raise NotImplementedError
+	 
