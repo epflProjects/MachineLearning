78e35de454d0a811312d7fba1768a24d92bb067d
Arnaud Pannatier <arn.pannatier@gmail.com>
2017-10-10 17:45:45 +0200

    Correction of SGD using Bastian's code

--- implementations.py
+++ implementations.py
@@ -35,21 +35,13 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     batch_size = 1
 
     for n_iter in range(max_iters):
-        #Stochastic choice
-        random_index = np.random.choice(range(len(y)), batch_size)
-
-        yp = y[random_index]
-        txp = tx[random_index]
-
-        #Gradient Descent
-        grad = compute_gradient(yp,txp,w)
-        
-        w = w-gamma*grad
+        for y_batch, tx_batch in batch_iter(y, tx, batch_size):
+            gradient = compute_gradient(y_batch, tx_batch, w)
+            w = w - gamma * gradient
     
     #Computation of the error    
     loss = compute_mse(y,tx,w)    
-    return (w, loss)    
-    
+    return (w, loss)
 
 def least_squares(y, tx):
     """Least squares regression using normal equations"""
@@ -102,6 +94,32 @@ def standardize(x):
     std_x = np.std(x)
     x = x / std_x
     return x, mean_x, std_x
+
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
 
 
