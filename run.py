abf8dc51f3755131546074698b8170954196868d
Arnaud Pannatier <arn.pannatier@gmail.com>
2017-10-29 13:19:51 +0100

    Edit run.py that normally works

--- run.py
+++ run.py
@@ -16,10 +16,11 @@ best_perf_of_columns = np.full((4), 0)
 min_poly_degree = 8
 max_poly_degree = 13
 
-y_clustered, tx_clustered, ids_clustered, test_y_clustered, test_tx_clustered, test_ids_clustered = prepare_clusters(data_train, data_test)
+y_clustered, normalTX, ids_clustered, test_y_clustered, test_tx_clustered, test_ids_clustered = prepare_clusters(data_train, data_test)
 
 for i in range(min_poly_degree, max_poly_degree+1):
-    coeffArr = np.full((4), i, dtype=int64)
+    tx_clustered = normalTX
+    coeffArr = np.full((4), i)
     tx_clustered, test_tx_clustered = preprocessing(tx_clustered, test_tx_clustered, coeffArr)
     w, perf_of_columns, predictions = search_best_polynomial_fit(i, y_clustered, tx_clustered, test_y_clustered, test_tx_clustered)
     for index, el in enumerate(perf_of_columns):
