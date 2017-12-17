e31af96232c4ba85a9efce7498da811bdca90890
bnanchen <nanchenbastian@gmail.com>
2017-12-17 22:12:19 +0100

    FLAGS amelioration and WIP

--- Project02/cnn/train.py
+++ Project02/cnn/train.py
@@ -14,8 +14,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 # Parameters
 # ==================================================
 
-positive_data = "./data/preprocess_train_pos.txt"
-negative_data = "./data/preprocess_train_neg.txt"
+positive_data = "./data/preprocess_train_pos_full.txt"
+negative_data = "./data/preprocess_train_neg_full.txt"
 
 embeddings_dimension = 200
 filter_sizes = "3,4,5"
@@ -28,9 +28,9 @@ epoch_numbers = 200
 
 
 # Data loading params
-tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("positive_data_file", "./data/preprocess_train_pos.txt", "Data source for the positive data.")
-tf.flags.DEFINE_string("negative_data_file", "./data/preprocess_train_neg.txt", "Data source for the negative data.")
+tf.flags.DEFINE_float("dev_sample_percentage", .05, "Percentage of the training data to use for validation") # TODO 0.1
+tf.flags.DEFINE_string("positive_data_file", "./data/preprocess_train_pos_full.txt", "Data source for the positive data.")
+tf.flags.DEFINE_string("negative_data_file", "./data/preprocess_train_neg_full.txt", "Data source for the negative data.")
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 200, "Dimensionality of character embedding (default: 128)")
@@ -42,9 +42,9 @@ tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularization lambda (default: 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("evaluate_every", 200, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
-tf.flags.DEFINE_integer("num_checkpoints", 5, "Number of checkpoints to store (default: 5)")
+tf.flags.DEFINE_integer("num_checkpoints", 60, "Number of checkpoints to store (default: 5)")
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
