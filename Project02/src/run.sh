f4eeb185b69332cafd1188e33bad9eba3a6b73d9
Alex Holloway <anthraxander@gmail.com>
2017-12-21 16:00:43 +0100

    Add run script to download data and get best predictions

---
+++ Project02/src/run.sh
@@ -0,0 +1,5 @@
+# Download the necessary train and test data, as well as a pre-trained model
+wget -O data.tgz https://drive.switch.ch/index.php/s/ij4XoRAJtGjBA8m/download && \
+  tar -cvzf data.tgz
+# Run the main script that generates our best predictions
+python main.py
